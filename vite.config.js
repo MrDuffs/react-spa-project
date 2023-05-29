76f0cfc0d6517643c164efe2c234742ccdce45eb
MrDuffs <chulkov.i@yandex.ru>
2023-05-29 21:01:50 +0300

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/posts',
+  server: {
+    host: 'localhost',
+    port: '8080',
+    proxy: {
+      '/api': {
+        target: 'https://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
