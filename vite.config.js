f37f7ca889e869946a4a73180cce44829ee06daa
MrDuffs <chulkov.i@yandex.ru>
2023-05-31 16:16:08 +0300

    remove some options from vite config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/posts',
   server: {
-    host: 'localhost',
     port: '8080',
     proxy: {
       '/api': {
