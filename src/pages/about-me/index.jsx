ec5aae7f9921fc8b282e184e19c0d53b95cfa55b
MrDuffs <chulkov.i@yandex.ru>
2023-05-31 18:06:58 +0300

    add about page

---
+++ src/pages/about-me/index.jsx
@@ -0,0 +1,12 @@
+import React, { memo } from 'react';
+import PageLayout from '../../components/page-layout';
+
+function AboutMe() {
+  return (
+    <PageLayout>
+      About Me
+    </PageLayout>
+  );
+}
+
+export default memo(AboutMe);
