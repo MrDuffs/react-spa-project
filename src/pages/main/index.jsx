6636d222cfc5b351cb6945f567e52c0e801a5aa1
MrDuffs <chulkov.i@yandex.ru>
2023-06-19 12:22:25 +0300

    dispatch load posts and select posts from store

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,22 +1,48 @@
-import { memo, useState } from 'react';
+import {
+  memo, useCallback, useEffect, useState,
+} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import PageLayout from '../../components/page-layout';
 import Header from '../../components/header';
 import Menu from '../../components/menu';
 import CardList from '../../components/card-list';
+import ItemCard from '../../components/item-card';
 
 function Main() {
+  const dispatch = useDispatch();
   const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    dispatch({
+      type: 'LOAD_POSTS',
+    });
+  }, [dispatch]);
+
+  const select = useSelector((state) => ({
+    posts: state.posts.data,
+    loading: state.posts.loading,
+  }));
+
+  // if (select) console.log(select.loading);
 
   const callbacks = {
     onShowMenu: () => setShow(true),
     onMenuClose: () => setShow(false),
   };
 
+  const renders = {
+    post: useCallback((item) => (
+      <ItemCard item={item} />
+    ), []),
+  };
+
   return (
     <PageLayout>
       <Header handleShow={callbacks.onShowMenu} />
       <Menu show={show} handleClose={callbacks.onMenuClose} />
-      <CardList />
+      {select.posts.length
+        ? <CardList list={select.posts} renderItem={renders.post} />
+        : 'Загрузка...'}
     </PageLayout>
   );
 }
