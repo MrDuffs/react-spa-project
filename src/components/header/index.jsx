5065eb09a507e04d648f355be62fcd3c322a26d7
MrDuffs <chulkov.i@yandex.ru>
2023-06-20 12:30:49 +0300

    edit header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,7 +3,7 @@ import { Container, Navbar } from 'react-bootstrap';
 
 function Header({ handleShow }) {
   return (
-    <Navbar bg="light" expand={false} className="mb-3">
+    <Navbar bg="light" expand={false}>
       <Container className="justify-content-start gap-2">
         <Navbar.Toggle onClick={handleShow} aria-controls={`offcanvasNavbar-expand-${false}`} />
         <Navbar.Brand>
