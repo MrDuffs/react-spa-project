265f4153a2515fd8957629c1447f2a2f50acd601
MrDuffs <chulkov.i@yandex.ru>
2023-06-20 12:31:36 +0300

    edit card list

--- src/components/card-list/index.jsx
+++ src/components/card-list/index.jsx
@@ -3,16 +3,13 @@ import { Col, Container, Row } from 'react-bootstrap';
 
 function CardList({ list, renderItem }) {
   return (
-    <Container>
+    <Container className="d-flex flex-column gap-4 mt-4 mb-4">
       {list.map((item) => (
-        <>
-          <Row key={item.id}>
-            <Col>
-              {renderItem(item)}
-            </Col>
-          </Row>
-          <br />
-        </>
+        <Row key={item.id}>
+          <Col>
+            {renderItem(item)}
+          </Col>
+        </Row>
       ))}
     </Container>
   );
