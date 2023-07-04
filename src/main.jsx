b396af554fde22b3401d6bc0df7b11fca698292e
MrDuffs <chulkov.i@yandex.ru>
2023-07-04 13:58:58 +0300

    remove strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,11 @@ import App from './pages';
 import store from './redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  // </React.StrictMode>,
 );
